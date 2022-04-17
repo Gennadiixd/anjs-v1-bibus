89756e6da44491833946e4ba43fc83d78ef0781e
Gennadiixd <gennadiixd@gmail.com>
2022-04-17 20:01:07 +0400

    add tracing, shutdown

--- main-back/src/apps/querier/index.ts
+++ main-back/src/apps/querier/index.ts
@@ -1,10 +1,30 @@
+// eslint-disable-next-line import/order
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// eslint-disable-next-line import/order
+import { initTracing } from "./tracing";
+
+initTracing({
+  // consoleTracerEnabled: !!process.env.CONSOLE_TRACER_ENABLED,
+  // tracingEnabled: !!process.env.TRACING_ENABLED,
+  consoleTracerEnabled: true,
+  tracingEnabled: true,
+  serviceName: process.env.SERVICE_NAME || "querier",
+  env: process.env.NODE_ENV,
+  serviceVersion: process.env.SERVICE_VERSION,
+});
+
 import knex from "knex";
 import { initLogger, pinoLogger } from "libs/@bibus/logger";
 
 import { asyncDbLocalStorage } from "../../libs/@bibus/db-main/knex-context";
+import { ReactiveCounter } from "../../libs/reactive-counter";
 
 import { config } from "./config";
 import { app } from "./fastify-app";
+import { gracefulShutdownC } from "./gracefull-shutdown";
 
 const main = async () => {
   // . LOGGER
@@ -24,8 +44,33 @@ const main = async () => {
     searchPath: ["knex", "public"],
   });
 
+  // . REACTIVE COUNTER
+  const reactiveCounter = ReactiveCounter.new();
+
+  app.addHook("onRequest", (request, reply, done) => {
+    reactiveCounter.increment();
+    done();
+  });
+
+  app.addHook("onResponse", (request, reply, done) => {
+    reactiveCounter.decrement();
+    done();
+  });
+
   await asyncDbLocalStorage.run({ knexConnection }, async () => {
     await app.listen(config.querier.http.port, "0.0.0.0");
+  });
+
+  const gracefulShutdown = gracefulShutdownC(knexConnection, reactiveCounter);
+
+  process.on("SIGINT", () => {
+    app.close();
+    gracefulShutdown("SIGINT");
+  });
+
+  process.on("SIGTERM", () => {
+    app.close();
+    gracefulShutdown("SIGTERM");
   });
 };
 
---
+++ main-back/src/libs/reactive-counter.ts
@@ -0,0 +1,53 @@
+export type ReactiveCounter = ReturnType<typeof ReactiveCounter.new>;
+
+export const ReactiveCounter = {
+  new: (initialValue: number = 0, zeroAndGreater: boolean = true) => {
+    let value = initialValue;
+    const subscribers: Array<(newVal: number, oldVal: number) => any> = [];
+
+    const set = (newValue: number) => {
+      if (zeroAndGreater && newValue < 0) {
+        return;
+      }
+
+      const oldVal = value;
+      value = newValue;
+
+      for (let i = 0; i < subscribers.length; i++) {
+        subscribers[i](newValue, oldVal);
+      }
+    };
+
+    const subscribe = (fn: (newVal: number, oldVal: number) => any) => {
+      subscribers.push(fn);
+    };
+
+    return {
+      value: () => value,
+      subscribe,
+      set,
+      add: (addVal: number) => {
+        set(value + addVal);
+      },
+      increment: () => {
+        set(value + 1);
+      },
+      decrement: () => {
+        set(value - 1);
+      },
+      waitUntil: async (valBecame: number): Promise<true> => {
+        if (value === valBecame) {
+          return true;
+        }
+
+        return new Promise((resolve) => {
+          subscribe((newVal) => {
+            if (newVal === valBecame) {
+              resolve(true);
+            }
+          });
+        });
+      },
+    };
+  },
+};
